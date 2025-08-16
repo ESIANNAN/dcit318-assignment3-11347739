625d3101797e267b32fe2f321a12dd935e258c97
ESIANNAN <pannan027@st.ug.edu.gh>
2025-08-16 05:09:42 +0000

    Update MobileMoneyProcessor.cs

--- Question1/MobileMoneyProcessor.cs
+++ Question1/MobileMoneyProcessor.cs
@@ -1,1 +1,10 @@
+using System;
+
+public class MobileMoneyProcessor : ITransactionProcessor
+{
+    public void Process(Transaction transaction)
+    {
+        Console.WriteLine($"Mobile Money: Processed {transaction.Amount:C} for {transaction.Category}");
+    }
+}
 
