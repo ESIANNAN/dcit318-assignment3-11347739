86dc6ee5f5f9a94d8900eb472f96bd1109334f42
ESIANNAN <pannan027@st.ug.edu.gh>
2025-08-16 05:24:38 +0000

    Update Program.cs
    Add Program.cs with Main method to run HealthSystemApp

--- Question 2/Program.cs
+++ Question 2/Program.cs
@@ -1,1 +1,15 @@
 
+class Program
+{
+    static void Main()
+    {
+        HealthSystemApp app = new HealthSystemApp();
+
+        app.SeedData();
+        app.BuildPrescriptionMap();
+        app.PrintAllPatients();
+
+        // example is displaying prescriptions for patient with ID = 2
+        app.PrintPrescriptionsForPatient(2);
+    }
+}
