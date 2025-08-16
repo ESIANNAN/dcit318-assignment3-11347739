edf105f77a1a567fb390a21e9f488efdb593cc1d
ESIANNAN <pannan027@st.ug.edu.gh>
2025-08-16 05:07:34 +0000

    Update CryptoWalletProcessor.cs
    Implement CryptoWalletProcessor with Process method

--- Question1/CryptoWalletProcessor.cs
+++ Question1/CryptoWalletProcessor.cs
@@ -1,1 +1,9 @@
+using System;
 
+public class CryptoWalletProcessor : ITransactionProcessor
+{
+    public void Process(Transaction transaction)
+    {
+        Console.WriteLine($"Crypto Wallet: Processed {transaction.Amount:C} for {transaction.Category}");
+    }
+}
