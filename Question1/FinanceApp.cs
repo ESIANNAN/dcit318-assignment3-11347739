ef14f2f5ea8109a27113a8742ad2d40d04f52a17
ESIANNAN <pannan027@st.ug.edu.gh>
2025-08-16 05:08:29 +0000

    Update FinanceApp.cs
    Implementing FinanceApp: instantiate account, create transactions, process and apply transactions

--- Question1/FinanceApp.cs
+++ Question1/FinanceApp.cs
@@ -1,1 +1,32 @@
+using System;
+using System.Collections.Generic;
 
+public class FinanceApp
+{
+    private List<Transaction> _transactions = new List<Transaction>();
+
+    public void Run()
+    {
+        SavingsAccount myAccount = new SavingsAccount("ACC12345", 1000m);
+
+        Transaction t1 = new Transaction(1, DateTime.Now, 150m, "Groceries");
+        Transaction t2 = new Transaction(2, DateTime.Now, 200m, "Utilities");
+        Transaction t3 = new Transaction(3, DateTime.Now, 500m, "Entertainment");
+
+        ITransactionProcessor mobileProcessor = new MobileMoneyProcessor();
+        ITransactionProcessor bankProcessor = new BankTransferProcessor();
+        ITransactionProcessor cryptoProcessor = new CryptoWalletProcessor();
+
+        mobileProcessor.Process(t1);
+        bankProcessor.Process(t2);
+        cryptoProcessor.Process(t3);
+
+        myAccount.ApplyTransaction(t1);
+        myAccount.ApplyTransaction(t2);
+        myAccount.ApplyTransaction(t3);
+
+        _transactions.Add(t1);
+        _transactions.Add(t2);
+        _transactions.Add(t3);
+    }
+}
